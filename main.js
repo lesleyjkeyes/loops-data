bc2dbe783ffebf2ae99ce945f8d00a10e00c79e5
Lesley Keyes <lesleyjkeyes@gmail.com>
2022-04-18 19:31:27 -0500

    added a button

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
 console.log("loops-data")
+const button = document.getElementById("main-btn")
+console.log(button)
+
+
+
+const button = document.getElementById("Please Click");
+console.log(button)
